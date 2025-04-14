541fb84dfeab73f598ba6b62a7c12d2370d3d5b4
Izzat Alsharif <work@izzatalsharif.com>
2025-04-14 23:35:07 +0300

    Add basic movement

--- src/movement/basic.cpp
+++ src/movement/basic.cpp
@@ -3,20 +3,47 @@
 // Basic motor controls
 void moveMotor(int motorIndex, int speed, bool forward)
 {
-    // Implementation will go here
+    int pin1 = motorPins[motorIndex * 2];
+    int pin2 = motorPins[motorIndex * 2 + 1];
+    int enPin = enablePins[motorIndex];
+
+    analogWrite(enPin, speed); // Set motor speed
+
+    if (forward)
+    {
+        digitalWrite(pin1, HIGH);
+        digitalWrite(pin2, LOW);
+    }
+    else
+    {
+        digitalWrite(pin1, LOW);
+        digitalWrite(pin2, HIGH);
+    }
 }
 
 void stopMotor(int motorIndex)
 {
-    // Implementation will go here
+    int pin1 = motorPins[motorIndex * 2];
+    int pin2 = motorPins[motorIndex * 2 + 1];
+    int enPin = enablePins[motorIndex];
+
+    digitalWrite(pin1, LOW);
+    digitalWrite(pin2, LOW);
+    analogWrite(enPin, 0);
 }
 
 void stopAllMotors()
 {
-    // Implementation will go here
+    stopMotor(0);
+    stopMotor(1);
+    stopMotor(2);
+    stopMotor(3);
 }
 
 void moveServo(int servoIndex, int angle)
 {
-    // Implementation will go here
+    if (servoIndex >= 0 && servoIndex < 2)
+    {
+        servos[servoIndex].write(angle);
+    }
 }
